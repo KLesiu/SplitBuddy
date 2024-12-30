fd1231555d2b166f1b902cc132052906e60f6bcc
Amtoni <dsw52717@student.dsw.edu.pl>
2024-12-30 14:26:24 +0100

    drobne poprawki pól tekstowych oraz zmiana design

--- mobile/lib/components/login.dart
+++ mobile/lib/components/login.dart
@@ -3,69 +3,212 @@ import 'package:split_buddy/services/navigatorService.dart';
 import 'home.dart';
 
 class Login extends StatelessWidget {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
   final TextEditingController usernameController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
+
+  void handleLogin(BuildContext context) {
+    if (_formKey.currentState!.validate()) {
+      // Jeśli walidacja pól się powiedzie, przenosimy na ekran główny
+      NavigatorService.navigateTo(context, Home());
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       appBar: AppBar(
         title: Text('Sign In'),
+        backgroundColor: Colors.green[900],
       ),
       body: Center(
         child: Padding(
           padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'Login Screen',
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-              ),
-              SizedBox(height: 20),
-              TextField(
-                controller: usernameController,
-                decoration: InputDecoration(
-                  labelText: 'Username',
-                  border: OutlineInputBorder(),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Login Screen',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.green[300],
+                  ),
                 ),
-              ),
-              SizedBox(height: 10),
-              TextField(
-                controller: passwordController,
-                decoration: InputDecoration(
-                  labelText: 'Password',
-                  border: OutlineInputBorder(),
-                ),
-                obscureText: true,
-              ),
-              SizedBox(height: 20),
-              Center(
-                child: ElevatedButton(
-                  onPressed: () {
-                    final username = usernameController.text;
-                    final password = passwordController.text;
-
-                    if (username.isEmpty || password.isEmpty) {
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        SnackBar(content: Text('Please fill in all fields')),
-                      );
-                    } else {
-                      // Po zalogowaniu przenosimy na ekran główny
-                      NavigatorService.navigateTo(context, Home());
+                SizedBox(height: 20),
+                TextFormField(
+                  controller: usernameController,
+                  decoration: InputDecoration(
+                    labelText: 'Username',
+                    labelStyle: TextStyle(color: Colors.green[300]),
+                    border: OutlineInputBorder(),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.green[300]!),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.green[500]!),
+                    ),
+                  ),
+                  style: TextStyle(color: Colors.white),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter a username';
                     }
+                    return null;
                   },
-                  child: Text('Login'),
                 ),
-              ),
-            ],
+                SizedBox(height: 10),
+                TextFormField(
+                  controller: passwordController,
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                    labelStyle: TextStyle(color: Colors.green[300]),
+                    border: OutlineInputBorder(),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.green[300]!),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.green[500]!),
+                    ),
+                  ),
+                  style: TextStyle(color: Colors.white),
+                  obscureText: true,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter a password';
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(height: 20),
+                SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.amber[700],
+                      foregroundColor: Colors.black,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.zero,
+                      ),
+                    ),
+                    onPressed: () => handleLogin(context),
+                    child: Text('Login'),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
     );
   }
 }
+
+
+
+
+
+// import 'package:flutter/material.dart';
+// import 'package:split_buddy/services/navigatorService.dart';
+// import 'home.dart';
+//
+// class Login extends StatelessWidget {
+//   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+//   final TextEditingController usernameController = TextEditingController();
+//   final TextEditingController passwordController = TextEditingController();
+//
+//   void handleLogin(BuildContext context) {
+//     if (_formKey.currentState!.validate()) {
+//       final username = usernameController.text;
+//       final password = passwordController.text;
+//
+//       NavigatorService.navigateTo(context, Home());
+//     }
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Sign In'),
+//       ),
+//       body: Center(
+//         child: Padding(
+//           padding: const EdgeInsets.all(16.0),
+//           child: Form(
+//             key: _formKey,
+//             child: Column(
+//               mainAxisAlignment: MainAxisAlignment.center,
+//               crossAxisAlignment: CrossAxisAlignment.start,
+//               children: [
+//                 Text(
+//                   'Login Screen',
+//                   style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+//                 ),
+//                 SizedBox(height: 20),
+//                 TextFormField(
+//                   controller: usernameController,
+//                   decoration: InputDecoration(
+//                     labelText: 'Username',
+//                     border: OutlineInputBorder(),
+//                   ),
+//                   validator: (value) {
+//                     if (value == null || value.isEmpty) {
+//                       return 'Please enter a username';
+//                     }
+//                     return null;
+//                   },
+//                 ),
+//                 SizedBox(height: 10),
+//                 TextFormField(
+//                   controller: passwordController,
+//                   decoration: InputDecoration(
+//                     labelText: 'Password',
+//                     border: OutlineInputBorder(),
+//                   ),
+//                   obscureText: true,
+//                   validator: (value) {
+//                     if (value == null || value.isEmpty) {
+//                       return 'Please enter a password';
+//                     }
+//                     if (value.length < 6) {
+//                       return 'Password must be at least 6 characters long';
+//                     }
+//                     return null;
+//                   },
+//                 ),
+//                 SizedBox(height: 20),
+//                 Center(
+//                   child: SizedBox(
+//                     width: double.infinity,
+//                     child: ElevatedButton(
+//                       style: ElevatedButton.styleFrom(
+//                         shape: RoundedRectangleBorder(
+//                           borderRadius: BorderRadius.zero,
+//                         ),
+//                       ),
+//                       onPressed: () => handleLogin(context),
+//                       child: Text('Login'),
+//                     ),
+//                   ),
+//                 ),
+//               ],
+//             ),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+
+
+
 
 
 
--- mobile/lib/components/register.dart
+++ mobile/lib/components/register.dart
@@ -10,14 +10,8 @@ class Register extends StatelessWidget {
   final TextEditingController passwordController = TextEditingController();
   final TextEditingController confirmPasswordController = TextEditingController();
 
-  // Funkcja obsługująca logikę rejestracji
   void handleRegister(BuildContext context) {
     if (_formKey.currentState!.validate()) {
-      final username = usernameController.text;
-      final email = emailController.text;
-      final password = passwordController.text;
-
-      // Po poprawnej walidacji przenosimy na ekran logowania
       NavigatorService.navigateTo(context, Login());
     }
   }
@@ -25,8 +19,10 @@ class Register extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       appBar: AppBar(
         title: Text('Sign up'),
+        backgroundColor: Colors.green[900],
       ),
       body: Center(
         child: Padding(
@@ -39,15 +35,27 @@ class Register extends StatelessWidget {
               children: [
                 Text(
                   'Register Screen',
-                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.green[300],
+                  ),
                 ),
                 SizedBox(height: 20),
                 TextFormField(
                   controller: usernameController,
                   decoration: InputDecoration(
                     labelText: 'Username',
+                    labelStyle: TextStyle(color: Colors.green[300]),
                     border: OutlineInputBorder(),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.green[300]!),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.green[500]!),
+                    ),
                   ),
+                  style: TextStyle(color: Colors.white),
                   validator: (value) {
                     if (value == null || value.isEmpty) {
                       return 'Please enter a username';
@@ -60,8 +68,16 @@ class Register extends StatelessWidget {
                   controller: emailController,
                   decoration: InputDecoration(
                     labelText: 'Email',
+                    labelStyle: TextStyle(color: Colors.green[300]),
                     border: OutlineInputBorder(),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.green[300]!),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.green[500]!),
+                    ),
                   ),
+                  style: TextStyle(color: Colors.white),
                   keyboardType: TextInputType.emailAddress,
                   validator: (value) {
                     if (value == null || value.isEmpty) {
@@ -78,8 +94,16 @@ class Register extends StatelessWidget {
                   controller: passwordController,
                   decoration: InputDecoration(
                     labelText: 'Password',
+                    labelStyle: TextStyle(color: Colors.green[300]),
                     border: OutlineInputBorder(),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.green[300]!),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.green[500]!),
+                    ),
                   ),
+                  style: TextStyle(color: Colors.white),
                   obscureText: true,
                   validator: (value) {
                     if (value == null || value.isEmpty) {
@@ -96,8 +120,16 @@ class Register extends StatelessWidget {
                   controller: confirmPasswordController,
                   decoration: InputDecoration(
                     labelText: 'Confirm Password',
+                    labelStyle: TextStyle(color: Colors.green[300]),
                     border: OutlineInputBorder(),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.green[300]!),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.green[500]!),
+                    ),
                   ),
+                  style: TextStyle(color: Colors.white),
                   obscureText: true,
                   validator: (value) {
                     if (value == null || value.isEmpty) {
@@ -111,9 +143,19 @@ class Register extends StatelessWidget {
                 ),
                 SizedBox(height: 20),
                 Center(
-                  child: ElevatedButton(
-                    onPressed: () => handleRegister(context),
-                    child: Text('Sign Up'),
+                  child: SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.amber[700],
+                        foregroundColor: Colors.black,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.zero,
+                        ),
+                      ),
+                      onPressed: () => handleRegister(context),
+                      child: Text('Sign Up'),
+                    ),
                   ),
                 ),
               ],
@@ -128,5 +170,273 @@ class Register extends StatelessWidget {
 
 
 
-
-
+// import 'package:flutter/material.dart';
+// import 'package:split_buddy/services/navigatorService.dart';
+// import 'login.dart';
+//
+// class Register extends StatelessWidget {
+//   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+//
+//   final TextEditingController usernameController = TextEditingController();
+//   final TextEditingController emailController = TextEditingController();
+//   final TextEditingController passwordController = TextEditingController();
+//   final TextEditingController confirmPasswordController = TextEditingController();
+//
+//   // Funkcja obsługująca logikę rejestracji
+//   void handleRegister(BuildContext context) {
+//     if (_formKey.currentState!.validate()) {
+//       final username = usernameController.text;
+//       final email = emailController.text;
+//       final password = passwordController.text;
+//
+//       // Po poprawnej walidacji przenosimy na ekran logowania
+//       NavigatorService.navigateTo(context, Login());
+//     }
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Sign up'),
+//       ),
+//       body: Center(
+//         child: Padding(
+//           padding: const EdgeInsets.all(16.0),
+//           child: Form(
+//             key: _formKey,
+//             child: Column(
+//               mainAxisAlignment: MainAxisAlignment.center,
+//               crossAxisAlignment: CrossAxisAlignment.start,
+//               children: [
+//                 Text(
+//                   'Register Screen',
+//                   style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+//                 ),
+//                 SizedBox(height: 20),
+//                 TextFormField(
+//                   controller: usernameController,
+//                   decoration: InputDecoration(
+//                     labelText: 'Username',
+//                     border: OutlineInputBorder(),
+//                   ),
+//                   validator: (value) {
+//                     if (value == null || value.isEmpty) {
+//                       return 'Please enter a username';
+//                     }
+//                     return null;
+//                   },
+//                 ),
+//                 SizedBox(height: 10),
+//                 TextFormField(
+//                   controller: emailController,
+//                   decoration: InputDecoration(
+//                     labelText: 'Email',
+//                     border: OutlineInputBorder(),
+//                   ),
+//                   keyboardType: TextInputType.emailAddress,
+//                   validator: (value) {
+//                     if (value == null || value.isEmpty) {
+//                       return 'Please enter an email';
+//                     }
+//                     if (!RegExp(r'^[^@]+@[^@]+\.[^@]+').hasMatch(value)) {
+//                       return 'Please enter a valid email address';
+//                     }
+//                     return null;
+//                   },
+//                 ),
+//                 SizedBox(height: 10),
+//                 TextFormField(
+//                   controller: passwordController,
+//                   decoration: InputDecoration(
+//                     labelText: 'Password',
+//                     border: OutlineInputBorder(),
+//                   ),
+//                   obscureText: true,
+//                   validator: (value) {
+//                     if (value == null || value.isEmpty) {
+//                       return 'Please enter a password';
+//                     }
+//                     if (value.length < 6) {
+//                       return 'Password must be at least 6 characters long';
+//                     }
+//                     return null;
+//                   },
+//                 ),
+//                 SizedBox(height: 10),
+//                 TextFormField(
+//                   controller: confirmPasswordController,
+//                   decoration: InputDecoration(
+//                     labelText: 'Confirm Password',
+//                     border: OutlineInputBorder(),
+//                   ),
+//                   obscureText: true,
+//                   validator: (value) {
+//                     if (value == null || value.isEmpty) {
+//                       return 'Please confirm your password';
+//                     }
+//                     if (value != passwordController.text) {
+//                       return 'Passwords do not match';
+//                     }
+//                     return null;
+//                   },
+//                 ),
+//                 SizedBox(height: 20),
+//                 Center(
+//                   child: SizedBox(
+//                     width: double.infinity,
+//                     child: ElevatedButton(
+//                       style: ElevatedButton.styleFrom(
+//                         shape: RoundedRectangleBorder(
+//                           borderRadius: BorderRadius.zero,
+//                         ),
+//                       ),
+//                       onPressed: () => handleRegister(context),
+//                       child: Text('Sign Up'),
+//                     ),
+//                   ),
+//                 ),
+//               ],
+//             ),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+//
+//
+//
+//
+// // import 'package:flutter/material.dart';
+// // import 'package:split_buddy/services/navigatorService.dart';
+// // import 'login.dart';
+// //
+// // class Register extends StatelessWidget {
+// //   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+// //
+// //   final TextEditingController usernameController = TextEditingController();
+// //   final TextEditingController emailController = TextEditingController();
+// //   final TextEditingController passwordController = TextEditingController();
+// //   final TextEditingController confirmPasswordController = TextEditingController();
+// //
+// //   // Funkcja obsługująca logikę rejestracji
+// //   void handleRegister(BuildContext context) {
+// //     if (_formKey.currentState!.validate()) {
+// //       final username = usernameController.text;
+// //       final email = emailController.text;
+// //       final password = passwordController.text;
+// //
+// //       // Po poprawnej walidacji przenosimy na ekran logowania
+// //       NavigatorService.navigateTo(context, Login());
+// //     }
+// //   }
+// //
+// //   @override
+// //   Widget build(BuildContext context) {
+// //     return Scaffold(
+// //       appBar: AppBar(
+// //         title: Text('Sign up'),
+// //       ),
+// //       body: Center(
+// //         child: Padding(
+// //           padding: const EdgeInsets.all(16.0),
+// //           child: Form(
+// //             key: _formKey,
+// //             child: Column(
+// //               mainAxisAlignment: MainAxisAlignment.center,
+// //               crossAxisAlignment: CrossAxisAlignment.start,
+// //               children: [
+// //                 Text(
+// //                   'Register Screen',
+// //                   style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+// //                 ),
+// //                 SizedBox(height: 20),
+// //                 TextFormField(
+// //                   controller: usernameController,
+// //                   decoration: InputDecoration(
+// //                     labelText: 'Username',
+// //                     border: OutlineInputBorder(),
+// //                   ),
+// //                   validator: (value) {
+// //                     if (value == null || value.isEmpty) {
+// //                       return 'Please enter a username';
+// //                     }
+// //                     return null;
+// //                   },
+// //                 ),
+// //                 SizedBox(height: 10),
+// //                 TextFormField(
+// //                   controller: emailController,
+// //                   decoration: InputDecoration(
+// //                     labelText: 'Email',
+// //                     border: OutlineInputBorder(),
+// //                   ),
+// //                   keyboardType: TextInputType.emailAddress,
+// //                   validator: (value) {
+// //                     if (value == null || value.isEmpty) {
+// //                       return 'Please enter an email';
+// //                     }
+// //                     if (!RegExp(r'^[^@]+@[^@]+\.[^@]+').hasMatch(value)) {
+// //                       return 'Please enter a valid email address';
+// //                     }
+// //                     return null;
+// //                   },
+// //                 ),
+// //                 SizedBox(height: 10),
+// //                 TextFormField(
+// //                   controller: passwordController,
+// //                   decoration: InputDecoration(
+// //                     labelText: 'Password',
+// //                     border: OutlineInputBorder(),
+// //                   ),
+// //                   obscureText: true,
+// //                   validator: (value) {
+// //                     if (value == null || value.isEmpty) {
+// //                       return 'Please enter a password';
+// //                     }
+// //                     if (value.length < 6) {
+// //                       return 'Password must be at least 6 characters long';
+// //                     }
+// //                     return null;
+// //                   },
+// //                 ),
+// //                 SizedBox(height: 10),
+// //                 TextFormField(
+// //                   controller: confirmPasswordController,
+// //                   decoration: InputDecoration(
+// //                     labelText: 'Confirm Password',
+// //                     border: OutlineInputBorder(),
+// //                   ),
+// //                   obscureText: true,
+// //                   validator: (value) {
+// //                     if (value == null || value.isEmpty) {
+// //                       return 'Please confirm your password';
+// //                     }
+// //                     if (value != passwordController.text) {
+// //                       return 'Passwords do not match';
+// //                     }
+// //                     return null;
+// //                   },
+// //                 ),
+// //                 SizedBox(height: 20),
+// //                 Center(
+// //                   child: ElevatedButton(
+// //                     onPressed: () => handleRegister(context),
+// //                     child: Text('Sign Up'),
+// //                   ),
+// //                 ),
+// //               ],
+// //             ),
+// //           ),
+// //         ),
+// //       ),
+// //     );
+// //   }
+// // }
+// //
+// //
+// //
+// //
+// //
+// //
